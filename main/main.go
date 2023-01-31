577c1ec3d011844a5c973d9646524276ebd747e1
abdullah <110098187+acoplu@users.noreply.github.com>
2023-01-31 22:25:39 +0300

    Creating "user" Global Variable to Keep Purchases and Feedbacks

--- main/main.go
+++ main/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"CelikGroupCRUDAPI/models" //user and book struct
+	//"github.com/gin-gonic/gin" //golang http web framework
+)
+
+// define our User object to keep user's feedbacks and purchases
+var user = models.User{Feedbacks: nil, Purchases: nil}
